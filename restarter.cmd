6979de23e34d07c7061055218ed9d600026c366b
Nick <48698159+NickNotCool@users.noreply.github.com>
2020-01-04 19:58:00 -0500

    Update restarter.cmd

--- restarter.cmd
+++ restarter.cmd
@@ -2,20 +2,24 @@
 echo Starting Server Updater...
 echo Started!
 
+color a
 xcopy "bungee\LuckPerms-Bungee-5.0.51.jar" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins"
 
+color b
 md "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 xcopy "bungee\luckperms-h2.mv.db" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 xcopy "bungee\config.yml" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 xcopy "bungee\contexts.json" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 
+color c
 md "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms\lib"
 xcopy /s/h/e/k/f/c "bungee\lib" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms\lib"
 
 clear
-timeout -m 30
+timeout 2
+color f
 echo Done Bungee!
-timeout -m 100
+timeout 2
 
 
 pause
