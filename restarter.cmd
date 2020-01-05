d83ce8823f4ae02935609505aea8256791fef47d
Nick <48698159+NickNotCool@users.noreply.github.com>
2020-01-04 20:10:15 -0500

    Added CONFIG Files

--- restarter.cmd
+++ restarter.cmd
@@ -1,26 +1,33 @@
-@echo off
 echo Starting Server Updater...
 echo Started!
 
-color a
+
 xcopy "bungee\LuckPerms-Bungee-5.0.51.jar" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins"
 
-color b
+
 md "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 xcopy "bungee\luckperms-h2.mv.db" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 xcopy "bungee\config.yml" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 xcopy "bungee\contexts.json" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms"
 
-color c
+
 md "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms\lib"
 xcopy /s/h/e/k/f/c "bungee\lib" "%USERPROFILE%\Desktop\MistaGreyCat Network\Bungee\plugins\LuckPerms\lib"
 
-clear
-timeout 2
-color f
+
 echo Done Bungee!
 timeout 2
 
+echo Type A
+xcopy "creative\luckperms.conf" "%USERPROFILE%\Desktop\MistaGreyCat Network\Creative\config\luckperms
+echo Type A
+xcopy "factions\luckperms.conf" "%USERPROFILE%\Desktop\MistaGreyCat Network\Moded-Factions\config\luckperms
+echo Type A
+xcopy "lobby\luckperms.conf" "%USERPROFILE%\Desktop\MistaGreyCat Network\Lobby\config\luckperms
+
+
+echo Done Config-Editing!
+echo Bye.
+timeout 5
 
 pause
-
